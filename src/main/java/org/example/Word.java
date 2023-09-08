02fb8dfae162001d94274d2fa98758a41a4c7dec
Mingyu Lee <livingdavid@naver.com>
2023-09-08 10:15:38 +0900

    Make WordCRUD file

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -1,8 +1,50 @@
 package org.example;
 
 public class Word {
-    int no;
-    string level;
-    string engword;
-    string korword;
+
+    private int id;
+    private int level;
+    private string word;
+    private string meaning;
+
+    Word(){}
+    Word(int id, int level, String word, String meaning){
+        this.id = id;
+        this.level = level;
+        this.word= word;
+        this.meaning=meaning;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public string getWord() {
+        return word;
+    }
+
+    public void setWord(string word) {
+        this.word = word;
+    }
+
+    public string getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(string meaning) {
+        this.meaning = meaning;
+    }
+
+
 }
